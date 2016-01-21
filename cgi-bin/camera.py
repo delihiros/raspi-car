1c00bb34029c3236aa23e32d8eafd375f2d984bb
delihiros <delihiros@gmail.com>
2016-01-21 08:53:21 +0000

    first commit

---
+++ cgi-bin/camera.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import time
+import cv2
+
+def capture_camera(mirror=True, size=None):
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+
+    if mirror is True:
+        frame = frame[:,::-1]
+
+    if size is not None and len(size) == 2:
+        frame = cv2.resize(frame, size)
+
+    cv2.imwrite('./foo.jpg', frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+capture_camera()
+print "Content-type: text/html\n"
+print """
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Camera</title>
+  </head>
+  <body>
+    <img src="/foo.jpg">
+  </body>
+</html>
+"""
